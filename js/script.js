0fb8b7602acc41f9ebe5ade28a1af422b2252ce6
mondai40 <tvwmdaimon40@gmail.com>
2019-07-07 23:23:16 -0700

    add responsive css

--- js/script.js
+++ js/script.js
@@ -6,4 +6,25 @@ $(document).ready(function() {
             "scrollTop": position - 65
         }, 500);
     });
+
+    $(".skill").hover(
+        function() {
+            // $(this).find(".skill-icon").find("p").animate({"color": "red"}, 500);
+            $(this).find(".skill-icon").find("p").css("color", "blue");
+                // "font-size": 150%,
+                // "font-color": red,
+                // font-weight: bold
+            
+        }, 
+        function() {
+            // $(this).find(".skill-icon").find("p").animate({"color": "blue"}, 500);
+            $(this).find(".skill-icon").find("p").css("color", "blue");
+                // "font-size": 100%,
+                // "font-color": white,
+                // font-weight: normal
+            
+       }
+    );
+
+    
 });
